b4d6043f7f05d92c2b2efc263173d5ff95703526
nicolas <diy-videogame@nacq.me>
2023-11-25 17:50:24 +0100

    dialog test file

---
+++ dialogs.js
@@ -0,0 +1,44 @@
+var dialogs = {
+    "dialog_intro_3": {
+        "txt": "Test",
+    },
+    "dialog_intro_2": {
+        "txt": "Ha ça marche je crois",
+    },
+    "dialog_intro_1": {
+        "txt": "Allo ???",
+    },
+    "dialog_intro": {
+        "txt": "Y a quelqu'un ? Tu me reçois ?",
+        "choices": [
+            {
+                "txt": "Oui",
+                "goto": 2
+            },
+            {
+                "txt": "Non",
+                "goto": 42
+            }
+        ]
+    },
+    "dialog_1": {
+        "txt": "Hello",
+        "choices": [
+            {
+                "txt": "Oui",
+                "goto": 2
+            },
+            {
+                "txt": "Non",
+                "goto": 42
+            }
+        ]
+    },
+    "dialog_2": {
+        "txt": "Ca va pas",
+        "dialog_id": 1
+    },
+    "dialog_42": {
+        "txt": "test"
+    }
+}
